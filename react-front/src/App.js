5acfe742e07b464bcde63f4e7794c9dda663b851
Agustin Carretto <etemagus@gmail.com>
2023-09-12 00:31:34 -0300

    refactor LoginForm

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
 import LoginForm from "./LoginForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-      <LoginForm />
-  );
+      <div className="App">
+          <main>
+              <LoginForm /> {/* Renderiza el formulario de inicio de sesión aquí */}
+          </main>
+      </div>
+  )
 }
 
 export default App;
--- react-front/src/LoginForm.js
+++ react-front/src/LoginForm.js
@@ -25,36 +25,51 @@ function LoginForm() {
 
             console.log('Datos de inicio de sesión:', formData);
         }
+    };
 
         return (
-            <div>
-                <h2>Iniciar sesión</h2>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Email:</label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                        />
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">Iniciar Sesión</div>
+                            <div className="card-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="email" className="form-label">Email:</label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            id="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            placeholder="Ingresa tu email"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="password" className="form-label">Contraseña:</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            id="password"
+                                            name="password"
+                                            value={formData.password}
+                                            onChange={handleChange}
+                                            placeholder="Ingresa tu contraseña"
+                                            required
+                                        />
+                                    </div>
+                                    <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <label>Contraseña:</label>
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <button type="submit">Iniciar sesión</button>
-                </form>
+                </div>
             </div>
         );
-    }
+
 }
 
 export default LoginForm;
