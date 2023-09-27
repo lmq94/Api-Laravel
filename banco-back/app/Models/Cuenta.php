529d3531b78a4cc12e3c5e3dc75317d3915ed4ee
Lucas Quiroga <quiroga9409@gmail.com>
2023-09-27 03:59:49 -0300

    Add the cbu attribute in account

--- banco-back/app/Models/Cuenta.php
+++ banco-back/app/Models/Cuenta.php
@@ -6,13 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-;
+
 
 
 class Cuenta extends Model
 {
     use HasFactory;
-    protected $fillable = ['id','id_cliente','saldo', 'tipo_de_cuenta', 'moneda'];
+    protected $fillable = ['id',
+        'id_cliente',
+        'saldo',
+        'tipo_de_cuenta',
+        'moneda',
+        'cbu'];
 
     protected $hidden = [
         'id_cliente',
