15eafca0efa0a678d1aae841df62c7798a4a69fa
Lucas Quiroga <quiroga9409@gmail.com>
2023-09-28 22:10:27 -0300

    created the footer

---
+++ banco-front/src/templates/Footer.js
@@ -0,0 +1,12 @@
+
+function FatFooter  ()  {
+    return (
+        <footer className="bg-dark text-light text-center p-3 ">
+            <div className="container">
+                <p>&copy; 2023 Tu Empresa. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default FatFooter;
