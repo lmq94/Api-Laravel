fd7bb5dc2fdae1a06c3e1fcdf290660756724517
Lucas Quiroga <quiroga9409@gmail.com>
2023-09-28 22:11:25 -0300

    Refactor navbar

--- banco-front/src/templates/Header.js
+++ banco-front/src/templates/Header.js
@@ -3,9 +3,12 @@ import {Logout} from "../crud/Functions";
 import UserData from "../crud/UserData";
 import React from "react";
 
+
+
 function Header({ isLoggedIn, setIsLoggedInCallback, userRole } ){
 
     const navigate = useNavigate();
+
 
     const handleLogout = () => {
 
@@ -15,45 +18,52 @@ function Header({ isLoggedIn, setIsLoggedInCallback, userRole } ){
     };
 
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Bienvenidos </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">About</Link>
-                        </li>
-                        {userRole === "normal"&& isLoggedIn ? (
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/create-account">
-                                    Abrir una cuenta
-                                </Link>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+                <div className="container-fluid d-flex align-items-center">
+
+                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item border m-1">
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
-                        ) : null}
+                            <li className="nav-item border m-1">
+                                <Link className="nav-link" to="/">About</Link>
+                            </li>
+                            {userRole === "normal"&& isLoggedIn ? (
+                                <li className="nav-item border m-1">
+                                    <Link className="nav-link" to="/create-account">
+                                        Abrir una cuenta
+                                    </Link>
+                                </li>
+                            ) : null}
+                            {isLoggedIn ?(
+                                <li className = "nav-item border m-1">
+                                    <Link className="nav-link" to="/" onClick={() => Logout({ setIsLoggedInCallback })}>
+                                        Deslogearse
+                                    </Link>
+                                </li>
+                            ) : (
+                                <li className="nav-item border m-1">
+                                    <Link className="nav-link" to="/register">Registrarse</Link>
+                                </li>
+                            )}
+                        </ul>
+
+                    </div>
+                    <div className>
                         {isLoggedIn ?(
-                            <li className = "nav-item">
-                                <Link className="nav-link" to="/" onClick={() => Logout({ setIsLoggedInCallback })}>
-                                    Deslogearse
-                                </Link>
-                            </li>
-                        ) : (
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">Registrarse</Link>
-                            </li>
-                        )}
-                    </ul>
+                            <div className="position-absolute top-0 end-0 p-3">
+                                <UserData />
+                            </div>
+                        ) :null}
+                    </div>
                 </div>
-                <div className="ml-auto">
-                    <UserData />
-                </div>
-            </div>
-    </nav>)
+            </nav>
+        </header>)
 }
 
 export {Header}
