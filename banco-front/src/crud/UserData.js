7735edbe811ea02cafd4fcc917a199891e3fdf45
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-02 22:25:36 -0300

    Implemented UserData.js

--- banco-front/src/crud/UserData.js
+++ banco-front/src/crud/UserData.js
@@ -1,6 +1,9 @@
 import {useEffect, useState} from "react";
-import {axiosInstance} from "../AxiosConfig";
-import Modal from "bootstrap/js/src/modal";
+import {axiosInstance, setAuthToken} from "../AxiosConfig";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCog, faUser} from "@fortawesome/free-solid-svg-icons";
+import {useNavigate} from "react-router-dom";
+
 
 
 function UserData ({ show, onHide })  {
@@ -8,6 +11,9 @@ function UserData ({ show, onHide })  {
     const [showData, setShowData] = useState(false);
 
 
+    const image = userData.profile_picture;
+
+    const url = "http://localhost:8000/storage/"
 
 
      useEffect(() => {
@@ -26,24 +32,43 @@ function UserData ({ show, onHide })  {
         setShowData(!showData);
     };
 
+    const navigate = useNavigate();
+
+    const navigateToUserConfig = () => {
+        navigate('/user-config')
+    };
+
     return (
         <div>
             <button className="btn btn-primary" onClick={toggleData}>
-                Ver Datos
+                <FontAwesomeIcon icon={faUser} />
             </button>
             {showData && (
                 <div className="user-data-container bg-light p-4">
-                    {/* Contenido del contenedor */}
-                    <h2 className="mb-3">Datos del Usuario</h2>
-                    <p>Nombre: {userData.name}</p>
-                    {/* Otros datos del usuario */}
-                    <button className="btn btn-danger" onClick={toggleData}>
-                        Cerrar
-                    </button>
+                    <div className="card" style={{ maxWidth: "300px" }}>
+                        <div className="card-body">
+                            <img
+                                src={`${url}${image}`}
+                                alt="Foto de perfil"
+                                className="img-thumbnail"
+                                style={{ maxWidth: "100px" }} // Aquí se corrige
+                            />
+                            <h5 className="card-title">{userData.name}</h5>
+                            <p className="card-text">Correo electrónico: {userData.email}</p>
+                            <button className="btn btn-primary" onClick={navigateToUserConfig}>
+                                <FontAwesomeIcon icon={faCog} className="me-2"/>
+                                Configuración
+                            </button>
+                        </div>
+                        <button className="btn btn-danger" onClick={toggleData}>
+                            Cerrar
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
     );
+
 }
 
 export default UserData;
