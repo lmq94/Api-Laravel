2755320a57d6c01c1ea4831989218d6ab428cc18
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-02 22:29:58 -0300

    Implemented buttons to create entities

--- banco-front/src/crud/Panel.js
+++ banco-front/src/crud/Panel.js
@@ -2,19 +2,46 @@
 import TableItems from "./TableItems.js"
 import {setAuthToken} from "../AxiosConfig";
 import Cookies from "js-cookie";
-
+import {useNavigate} from "react-router-dom";
 
 function AdminPanel() {
+
+    const navigate = useNavigate();
+
+
+    const handleCrearCliente = () => {
+        // Aquí puedes manejar la lógica para crear un cliente
+        console.log('Crear cliente');
+      }
+      
+      function handleCrearUsuario() {
+        navigate('/register');
+      }
+      
+      function handleAbrirCuenta() {
+        navigate('/create-account');
+        
+      }
+
+
     return (
         <div>
             <h2>Cuentas en el banco</h2>
             <TableItems ruta="/cuentas" edicion={true}/>
             <h2>Clientes en el banco</h2>
             <TableItems ruta="/clientes" edicion={true}/>
-            <h2>Cuentas registradas en el banco</h2>
+            <h2>Usuarios en el banco</h2>
             <TableItems ruta="/users" edicion={true}/>
-        </div>
-    );
+
+            <div style={{ marginTop: '20px' }}>
+                <button className="btn btn-info ms-4" onClick={handleCrearCliente}>Crear Cliente</button>
+                <button className="btn btn-info ms-4" onClick={handleCrearUsuario}>Crear Usuario</button>
+                <button className="btn btn-info ms-4" onClick={handleAbrirCuenta}>Abrir Cuenta</button>
+            </div>
+    </div>
+  );
+        
+    
 }
 
 function UserPanel() {
@@ -25,7 +52,10 @@ function UserPanel() {
 
             <TableItems ruta="/clientes-cuentas"  edicion={false}/>
 
+            <p>Recuerda que si quieres dar de baja o modificar algunos datos de tu cuenta debes comunicarte con el administrador</p>
         </div>
+
+        
     );
 }
 
