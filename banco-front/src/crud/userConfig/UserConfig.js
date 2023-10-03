7febfe3a18f5f26c1c0b957bdac383f264ed7741
Lucas Quiroga <quiroga9409@gmail.com>
2023-10-02 22:09:26 -0300

    implemented visual improvements with bootstrap

--- banco-front/src/crud/userConfig/UserConfig.js
+++ banco-front/src/crud/userConfig/UserConfig.js
@@ -5,36 +5,47 @@ import EditUserDataForm from './EditUserDataForm';
 function UserConfig() {
     const [activeTab, setActiveTab] = useState(null);
     const [passwordChangeMessage, setPasswordChangeMessage] = useState('');
-
+  
     return (
-        <div>
-            <div>
-                <button onClick={() => setActiveTab('changePassword')}>Cambiar Contraseña</button>
-                <button onClick={() => setActiveTab('editUserData')}>Editar Datos</button>
+        <div className="text-center d-flex flex-column align-items-center justify-content-center mt-4">
+        {/* Título siempre visible */}
+        <h2 className="mb-4">Configuración de usuario</h2>
+      
+        <div className="sticky-top bg-white mb-4">
+  {/* Contenedor de botones fijado en la parte superior */}
+            <div className="row">
+                <div className="col-12 col-md-6 mb-2">
+                    <button onClick={() => setActiveTab('changePassword')} className="btn btn-primary  w-100 text-nowrap">Contraseña</button>
+                </div>
+                    <div className="col-12 col-md-6 mb-2">
+                <button onClick={() => setActiveTab('editUserData')} className="btn btn-primary btn-block w-100 text-nowrap">Editar Datos</button>
+                </div>
             </div>
-
-            {activeTab === 'changePassword' && (
-                <div>
-                    <h2>Cambiar Contraseña</h2>
-                    {passwordChangeMessage && (
-                        <p style={{ backgroundColor: passwordChangeMessage === 'success' ? 'green' : 'red' }}>
-                            {passwordChangeMessage === 'success'
-                                ? '¡Contraseña cambiada correctamente!'
-                                : 'Error: los datos ingresados no son correctos.'}
-                        </p>
-                    )}
-                    <ChangePasswordForm setPasswordChangeMessage={setPasswordChangeMessage} />
-                </div>
-            )}
-
-            {activeTab === 'editUserData' && (
-                <div>
-                    <h2>Editar Datos de Usuario</h2>
-                    <EditUserDataForm />
-                </div>
-            )}
         </div>
-    );
-}
-
-export default UserConfig;
+      
+        {/* Contenido dinámico */}
+        {activeTab === 'changePassword' && (
+          <div>
+            <h4>Cambiar Contraseña</h4>
+            {passwordChangeMessage && (
+              <p style={{ backgroundColor: passwordChangeMessage === 'success' ? 'green' : 'red' }}>
+                {passwordChangeMessage === 'success'
+                  ? '¡Contraseña cambiada correctamente!'
+                  : 'Error: los datos ingresados no son correctos.'}
+              </p>
+            )}
+            <ChangePasswordForm setPasswordChangeMessage={setPasswordChangeMessage} />
+          </div>
+        )}
+      
+        {activeTab === 'editUserData' && (
+          <div>
+            <h4>Editar Datos de Usuario</h4>
+            <EditUserDataForm />
+          </div>
+        )}
+      </div>
+      );
+    }
+    
+    export default UserConfig;
